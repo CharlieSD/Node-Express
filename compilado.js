99c0e14d0dff0ddbb4e7aaf8a1b7fd5c6ab7ac41
CharlieSD <Carlos.espinoza@debite.com.mx>
2018-09-06 13:13:57 -0500

    Manejo de Eventos

--- compilado.js
+++ compilado.js
@@ -1,9 +1,33 @@
-"use strict";
+'use strict';
 
-var value = 24;
+var _fs = require('fs');
 
-var miFn = function miFn(value) {
-  return value * 10;
-};
+var _fs2 = _interopRequireDefault(_fs);
 
-console.log(miFn(value));
+var _readline = require('readline');
+
+var _readline2 = _interopRequireDefault(_readline);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Muestra el número total de lineas, y el número de palabras por linea
+// Documentación: https://nodejs.org/api/readline.html
+const file = process.argv[2];
+let lines = 0;
+
+console.log(`Fichero selecionado: ${file}\n`);
+
+// Emite el evento 'line' cuando lee una linea
+const rl = _readline2.default.createInterface({
+  input: _fs2.default.createReadStream(file),
+  crlfDelay: Infinity
+});
+
+rl.on('line', line => {
+  ++lines;
+  console.log(`Número de caracteres por linea: ${line.length}`);
+});
+
+rl.on('close', () => {
+  console.log(`\nNúmero total de lineas: ${lines}`);
+});
