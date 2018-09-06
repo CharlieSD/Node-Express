3572bb3694971a2cc1b4de18d6299fc8fc2fe41b
CharlieSD <Carlos.espinoza@debite.com.mx>
2018-09-06 13:48:28 -0500

    Sockets

--- src/client.js
+++ src/client.js
@@ -4,5 +4,5 @@ const socket = net.Socket()
 socket.connect(8000, 'localhost')
 
 socket.write('Hola?')
-
+socket.on('connect', () => console.log('socket connected'))
 socket.on('data', data => console.log(data.toString()))
